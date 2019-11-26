108dda47db5b0c1a29ca544a2b634dc4bb1b2f22
Jann Deterling <jann.deterling@thoughtworks.com>
2019-11-26 13:45:35 +0100

    [Jann] adds starter of the web module

---
+++ src/starter/webStarter.ts
@@ -0,0 +1,16 @@
+
+
+export class WebStarter {
+
+  public async initializeModule(configuration: any): Promise<void>{
+
+  }
+  public async injectDependencies(instanceMap: Map<string, any>): Promise<void>{
+
+  }
+
+  public async shoutDownModule(): Promise<void>{
+
+  }
+
+}
