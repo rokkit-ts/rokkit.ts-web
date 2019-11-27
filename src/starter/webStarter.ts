9291da7a86c0642c641bab4456c3c4ddde6109cf
Jann Deterling <jann.deterling@thoughtworks.com>
2019-11-27 11:46:12 +0100

    [Jann] updates webStarter to use the restify httpserver

--- src/starter/webStarter.ts
+++ src/starter/webStarter.ts
@@ -1,7 +1,8 @@
-import {ControllerInformation} from "../component/util/controllerInformation";
-import {HttpServer} from "../http/server/httpServer";
-import {RestifyHttpServer} from "../http/server/restifyHttpServer";
-import {RequestHandlerFactory} from "../http/server/RequestHandlerFactory";
+import { ControllerInformation } from "../component/util/controllerInformation";
+import { RequestMapping } from "../component/util/request/requestMapping";
+import { HttpServer } from "../http/server/httpServer";
+import { RequestHandlerFactory } from "../http/server/requestHandlerFactory";
+import { RestifyHttpServer } from "../http/server/restifyHttpServer";
 
 const HTTP_CONTROLLERS: ControllerInformation[] = [];
 
@@ -10,26 +11,60 @@ export function addHttpController(controller: ControllerInformation) {
 }
 
 export class WebStarter {
-
   private httpServer: HttpServer | undefined;
   private requestHandlerFactory: RequestHandlerFactory;
 
-  constructor(){
+  constructor() {
     this.requestHandlerFactory = new RequestHandlerFactory();
   }
 
-  public async initializeModule(configuration: any): Promise<void>{
+  public async initializeModule(configuration: any): Promise<void> {
     this.httpServer = new RestifyHttpServer();
-     await this.httpServer.run();
-  }
-  public async injectDependencies(instanceMap: Map<string, any>): Promise<void>{
+    await this.httpServer.run();
   }
 
-  public async shoutDownModule(): Promise<void>{
-    if (this.httpServer){
+  public async injectDependencies(
+    instanceMap: Map<string, any>
+  ): Promise<void> {
+    Promise.all(
+      HTTP_CONTROLLERS.map(async controllerInformation => {
+        const controllerInstance: any = instanceMap.get(
+          controllerInformation.controllerName
+        );
+        await Promise.all(
+          controllerInformation.resourceMappings.map(requestMapping => {
+            this.addRequestHandlerFunctionToHttpServer(
+              controllerInstance,
+              requestMapping,
+              controllerInformation
+            );
+          })
+        );
+      })
+    );
+  }
+
+  public async shoutDownModule(): Promise<void> {
+    if (this.httpServer) {
       await this.httpServer.stop();
     }
   }
 
+  private addRequestHandlerFunctionToHttpServer(
+    controllerInstance: any,
+    requestMapping: RequestMapping,
+    controllerInformation: ControllerInformation
+  ) {
+    const handlerFunction = this.requestHandlerFactory.buildHandlerFunction(
+      controllerInstance,
+      requestMapping
+    );
+    if (this.httpServer) {
+      this.httpServer.addRequestHandler(
+        requestMapping.httpMethod,
+        controllerInformation.basePath + requestMapping.resourcePath,
+        handlerFunction
+      );
+    }
+  }
 }
-
