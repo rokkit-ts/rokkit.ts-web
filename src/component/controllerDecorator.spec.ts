4c73f58e405d760ba63fa25b0d82ce63c115253e
Jann Deterling <jann.deterling@thoughtworks.com>
2020-03-12 17:45:24 +0100

    [Jann] refactors controllerDecorator test to use jest

--- src/component/controllerDecorator.spec.ts
+++ src/component/controllerDecorator.spec.ts
@@ -1,68 +1,53 @@
 // tslint:disable:max-classes-per-file
 // tslint:disable:no-unused-expression
 
-import dependencyInjectionAssembler from "@rokkit.ts/dependency-injection/lib/dependency-injection-assembler/dependencyInjectionAssembler";
-import { expect } from "chai";
-import { suite, test } from "mocha-typescript";
-import * as sinon from "sinon";
-import { HttpMethod } from "../http";
-import * as starterModule from "../starter";
-import { Controller } from "./controllerDecorator";
-import { Post } from "./httpRequestDecorators";
-import { RequestBody } from "./httpRequestParameterDecorators";
-import { ControllerInformation } from "./util/controllerInformation";
-import { RequestParameterType } from "./util/request/requestParameterType";
+const registerHttpControllerMock = jest.fn()
 
-sinon
-  .stub(starterModule, "registerHttpController")
-  .callsFake((controllerInformation: ControllerInformation) => {
-    controllers.push(controllerInformation);
-  });
+jest.mock('../starter', () => ({
+  registerHttpController: registerHttpControllerMock
+}))
 
-const controllers: ControllerInformation[] = [];
-const requestPath = "/test";
-const fileName = __filename;
-const contextName = "TEST";
+const injectableMock = jest.fn()
+injectableMock.mockReturnValue(jest.fn)
+jest.mock('@rokkit.ts/dependency-injection', () => ({
+  Injectable: injectableMock
+}))
 
-@suite
-class ControllerDecoratorSpec {
-  @test
-  public shouldMarkClassAsController() {
-    const context = dependencyInjectionAssembler.retrieveContext(contextName);
-    const injector = context?.getInjector(TestControllerClass.name);
+import { HttpMethod } from '../http'
+import { Controller } from './controllerDecorator'
+import { Post } from './httpRequestDecorators'
+import { RequestBody } from './httpRequestParameterDecorators'
+import { ControllerInformation } from './util/controllerInformation'
+import { RequestParameterType } from './util/request/requestParameterType'
+describe('ControllerDecorator', () => {
+  it('should register a controller at the WebStarter', () => {
+    // given/when
+    const expectedController: ControllerInformation = {
+      basePath: '/hello',
+      controllerName: 'TestControllerClass',
+      resourceMappings: [
+        {
+          httpMethod: HttpMethod.POST,
+          methodName: 'postMethod',
+          parameters: [{ index: 0, key: '', type: RequestParameterType.BODY }],
+          resourcePath: '/world'
+        }
+      ]
+    }
+    // then
+    expect(registerHttpControllerMock).toHaveBeenCalledTimes(1)
+    expect(registerHttpControllerMock).toHaveBeenCalledWith(expectedController)
+  })
 
-    expect(context).to.not.be.undefined;
-    expect(injector).to.not.be.undefined;
-    expect(controllers).to.not.be.empty;
-    expect(controllers.length).to.be.eq(1);
-    expect(controllers[0].basePath).to.be.eq(requestPath);
-    expect(controllers[0].controllerName).to.be.eq(TestControllerClass.name);
-    expect(controllers[0].resourceMappings).to.not.be.undefined;
-    expect(controllers[0].resourceMappings).to.not.be.empty;
-    expect(controllers[0].resourceMappings[0]).to.not.be.undefined;
-    expect(controllers[0].resourceMappings[0].httpMethod).to.be.eq(
-      HttpMethod.POST
-    );
-    expect(controllers[0].resourceMappings[0].methodName).to.be.eq(
-      "postMethod"
-    );
-    expect(controllers[0].resourceMappings[0].resourcePath).to.be.eq(
-      requestPath
-    );
-    expect(controllers[0].resourceMappings[0].parameters).to.not.be.empty;
-    expect(controllers[0].resourceMappings[0].parameters[0]).to.not.be
-      .undefined;
-    expect(controllers[0].resourceMappings[0].parameters[0].index).to.be.eq(0);
-    expect(controllers[0].resourceMappings[0].parameters[0].key).to.be.eq("");
-    expect(controllers[0].resourceMappings[0].parameters[0].type).to.be.eq(
-      RequestParameterType.BODY
-    );
-  }
-}
+  it('should register an inject in the dependency injector context', () => {
+    expect(injectableMock).toBeCalledTimes(1)
+    expect(injectableMock).toBeCalledWith(__filename, 'contextName')
+  })
+})
 
-@Controller(requestPath, fileName, contextName)
+@Controller('/hello', __filename, 'contextName')
 class TestControllerClass {
-  @Post(requestPath)
+  @Post('/world')
   // tslint:disable-next-line:no-empty
   public postMethod(@RequestBody() body: any) {}
 }
